ac9aa34790b7dbc76663cfbeeeedb049443e5654
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2024-10-03 11:27:48 -0700

    Update ex05.js

--- ex05.js
+++ ex05.js
@@ -1,9 +1,9 @@
 // Instructions: Use lodash's reduce function to get the product of all numbers in the array.
 
-const _ = require('lodash');
+const _ = require("lodash");
 
 const numbers = [1, 2, 3, 4];
-
+const product = _.reduce(numbers, (result, num) => result * num);
 // your code here
 
 console.log(product); // Expected output: 24
