b03c2be120f663c2b57c1b38e2c48a7b13d1dc9a
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2024-10-03 11:24:21 -0700

    Update ex04.js

--- ex04.js
+++ ex04.js
@@ -1,8 +1,12 @@
 // Instructions: Use lodash's chain function to filter the even numbers from the array and then get their sum.
 
-const _ = require('lodash');
+const _ = require("lodash");
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const sumOfEvens = _.chain(numbers)
+  .filter((num) => num % 2 === 0)
+  .sum()
+  .value();
 
 // your code here
 
