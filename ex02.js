b2c1ff1c552c569b494d82e4755e8ecd1cc16a32
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2024-10-03 11:20:43 -0700

    Update ex02.js

--- ex02.js
+++ ex02.js
@@ -1,8 +1,9 @@
 // Instructions: Use lodash's filter function to get only the even numbers from the array.
 
-const _ = require('lodash');
+const _ = require("lodash");
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const evens = _.filter(numbers, (num) => num % 2 === 0);
 
 // your code here
 
